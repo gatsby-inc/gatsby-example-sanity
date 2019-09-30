385a5debc4655581e698c73140b539256383ba02
Dustin Schau <DSchau@users.noreply.github.com>
2019-09-30 03:08:50 -0700

    Update gatsby-config.js

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -13,6 +13,8 @@ module.exports = {
     {
       resolve: "gatsby-source-sanity",
       options: Object.assign({}, clientConfig.sanity, {
+        projectId: process.env.GATSBY_SANITY_PROJECT_ID || `925zuquf`,
+        dataset: process.env.GATSBY_SANITY_DATASET || `production`,
         token: process.env.GATSBY_SANITY_READ_TOKEN,
         watchMode: !isProd,
         overlayDrafts: !isProd
